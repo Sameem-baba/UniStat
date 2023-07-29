e9d0976c6d7389a14e9ee40497334b1848fb8943
Sameem <ssssameembaba@gmail.com>
2023-07-30 02:23:56 +0530

    fix

--- app/resources/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head({ params }: { params: { slug: string } }) {
-  return (
-    <>
-      <title>21consequences - Resources</title>
-      <link href="/Logo.png" rel="icon" />
-    </>
-  );
-}
